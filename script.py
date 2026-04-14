7feedfe488ea1c8e34d730fabcdc7edbbacfbce1
leonardocrizzi <contatoleonardo124@outlook.com>
2026-04-13 21:36:08 -0300

    Verificação de média adicionada

--- script.py
+++ script.py
@@ -2,5 +2,9 @@ n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 n3 = float(input("Digite a terceira nota: "))
 
-resultado = (n1 + n2 + n3) / 3
-print(f"Sua média é: {resultado:.1f}")
+media = (n1 + n2 + n3) / 3
+
+if media < 7:
+    print(f"REPROVADO!\nMédia: {media:.1f}")
+else:
+    print(f"APROVADO!\nMédia: {media:.1f}")
